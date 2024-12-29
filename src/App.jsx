843da79064d49326aa5488ebdda7433e7027e79e
Vagish <Vagishmlk@gmail.com>
2024-12-29 07:38:47 +0530

    npm packages setup: react+vite, tailwind, daisyui, react-router-dom, reduxjs/toolkit, react-redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <p className="text-xl"> devtinder</p>
+        </>
+    );
+}
+
+export default App;
