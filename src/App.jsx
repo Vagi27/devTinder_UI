a59a8f477b0a152cf69a2c0e323d411455d9c32e
Vagish <Vagishmlk@gmail.com>
2025-01-20 00:03:02 +0530

    new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Feed from "./components/Feed";
-import Profile from "./components/Profile"
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
-import store from "./utils/store"
+import store from "./utils/store";
+import Requests from "./components/Requests";
+import Connections from "./components/Connections";
+
 function App() {
     return (
         <Provider store={store}>
             <BrowserRouter basename="/">
                 <Routes>
                     <Route path="/" element={<Body />}>
-                        <Route path="/" element={<Feed/>} />
+                        <Route path="/" element={<Feed />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/feed" element={<Feed />} />
                         <Route path="/profile" element={<Profile />} />
+                        <Route path="/requests" element={<Requests />} />
+                        <Route path="/connections" element={<Connections />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
